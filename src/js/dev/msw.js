5a30c792b7732f69a3d72d28dce1c1f85a1959ea
dwightjack <marco.solazzi@gmail.com>
2023-04-17 14:54:39 +0900

    refactor

--- src/js/dev/msw.js
+++
@@ -1,9 +0,0 @@
-import { rest, setupWorker } from 'msw';
-import { handler } from '../../../netlify_functions/tzlist';
-
-export const worker = setupWorker(
-  rest.get('/api/tzlist', async (req, res, ctx) => {
-    const { body } = await handler();
-    return res(ctx.json(JSON.parse(body)));
-  }),
-);
